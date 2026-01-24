98c3040caf29c8db986868cbb0c81b93dd3c2f61
Ayush Joshi <ayushjoshi8008@gmail.com>
2026-01-24 22:48:12 +0545

    Day 3

--- homepage.js
+++ homepage.js
@@ -1,16 +1,44 @@
 const add = document.getElementById('ad');
 const list = document.getElementById('to-do_list');
-const input = document.getElementById('task')
+const input = document.getElementById('task');
 
-add.addEventListener("click", addtask);
 
 function addtask() {
-    const  taskText = task.value.trim();
-
+    const  taskText = input.value.trim();
+    let val = true;
     if (taskText == ''){
-        alert("Sorry the task input feild can't be empty");
+        alert("Sorry the task input field can't be empty");
+        val = false;
     }
-
+    
     const taskItems = document.createElement('li');
     taskItems.className = 'task-item';
+    list.appendChild(taskItems);
+    
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.name = 'box'
+    
+    checkbox.addEventListener('change', function(){
+        if(this.checked){
+            taskItems.classList.add('completed');
+        }
+        else{
+            taskItems.classList.remove('completed');
+        }
+    })
+    taskItems.appendChild(checkbox);
+    
+    
+    
+    const spanText = document.createElement('text');
+    spanText.className = 'input-text';
+    spanText.textContent = taskText;
+    taskItems.appendChild(spanText);
+    
+    
+  
 }
+
+input.value = '';
+add.addEventListener('click', addtask);
